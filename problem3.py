14a87a8b19463604482d064b2577f81a5fb84d1f
RojinaBajra <reney.roze987@gmail.com>
2015-08-27 08:47:10 +0545

    Added Problem3

---
+++ problem3.py
@@ -0,0 +1,23 @@
+def arraySumLoop():
+
+	a= [100,15,1945]
+	b=[8,254,85]
+	length = len(a)
+	print(length)
+	i=0
+	res=[]
+	while i<length:
+
+		c=a[i]+b[i]
+		res.append(c)
+		i+=1
+	return res
+
+
+print(arraySumLoop())
+
+
+
+
+
+
